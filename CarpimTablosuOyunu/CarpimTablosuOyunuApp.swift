45270a5e66f33530ebd3701117690a6e3c2ec0b2
Zehra Coskun <rzg>
2023-03-15 13:50:12 +0300

    Initial Commit

---
+++ CarpimTablosuOyunu/CarpimTablosuOyunuApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarpimTablosuOyunuApp.swift
+//  CarpimTablosuOyunu
+//
+//  Created by Zehra Coşkun on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarpimTablosuOyunuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
